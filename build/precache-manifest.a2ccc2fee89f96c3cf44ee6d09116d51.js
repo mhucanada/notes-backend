15ff70c9e92991cfb4b89e23081d46c42aa587bf
mhucanada <mhucanada1@gmail.com>
2020-08-19 12:44:53 -0400

    idk

---
+++ build/precache-manifest.a2ccc2fee89f96c3cf44ee6d09116d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bab104c6653c3935026bd31cac8c02d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac871b4d70867d1ecbf2",
+    "url": "/static/css/main.2c1f1f39.chunk.css"
+  },
+  {
+    "revision": "9aeade65fbc64f2b2f34",
+    "url": "/static/js/2.784046ba.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.784046ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac871b4d70867d1ecbf2",
+    "url": "/static/js/main.a556fbc7.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
