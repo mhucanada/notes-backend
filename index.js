d6cb5c34f957af82babc5d12c51ab05afa9c1b8c
mhucanada <mhucanada1@gmail.com>
2020-08-19 12:50:38 -0400

    changed variable

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
-
+app.use(express.static('build'))
 //this allows us to use the json-parser, which allows us to 
 // get data from the body property of the request object
 app.use(express.json())
-app.use(express.static('build'))
+
 
 
 let notes = [
