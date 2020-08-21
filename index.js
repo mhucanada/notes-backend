8ecf481ed73acb990c171d2d9d5b29c70b538427
mhucanada <mhucanada1@gmail.com>
2020-08-20 21:45:57 -0400

    added eslint

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ require('dotenv').config()
 const Note = require('./models/note')
 const cors = require('cors')
 
-
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-//this allows us to use the json-parser, which allows us to 
+//this allows us to use the json-parser, which allows us to
 // get data from the body property of the request object
 
-
-/* 
+/*
 This uses the createServer method of the http module to create
 a new web server. An event handler is registered to the server, which is called
 every time an http request is made to the server's address
@@ -26,94 +24,93 @@ const app = http.createServer((request, response) => {
   })
 */
 
-
-
 // the id is a parameter, so it needs the colon in front of it
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes.map(note => note.toJSON()))
-  })
+	Note.find({}).then((notes) => {
+		response.json(notes.map((note) => note.toJSON()))
+	})
 })
 
 app.post('/api/notes', (request, response) => {
-  const body = request.body
+	const body = request.body
 
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
+	if (body.content === undefined) {
+		return response.status(400).json({
+			error: 'content missing',
+		})
+	}
 
-  // this ensures that only the content we want is saved into the notes
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
+	// this ensures that only the content we want is saved into the notes
+	const note = new Note({
+		content: body.content,
+		important: body.important || false,
+		date: new Date(),
+	})
 
-
-  note.save().then(savedNote => {
-    response.json(savedNote.toJSON())
-  })
+	note.save().then((savedNote) => {
+		response.json(savedNote.toJSON())
+	})
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+	Note.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		// eslint-disable-next-line no-undef
+		.catch((error) => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id).then(note => {
-    if (note) {
-      response.json(note.toJSON())
-    } else {
-      response.status(404).end()
-    }
-  })
-    .catch(error => next(error))
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note.toJSON())
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch((error) => next(error))
 })
 
-
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+	const body = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+	const note = {
+		content: body.content,
+		important: body.important,
+	}
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote.toJSON())
-    })
-    .catch(error => next(error))
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then((updatedNote) => {
+			response.json(updatedNote.toJSON())
+		})
+		.catch((error) => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
 
-  next(error)
+	next(error)
 }
 app.use(errorHandler)
 
-/* 
-this part binds the http server assigned to the app variable, and to listen to 
+/*
+this part binds the http server assigned to the app variable, and to listen to
 all http requests sent to the port 3001
 */
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,28 +1,26 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+	console.log('Please provide the password as an argument: node mongo.js <password>')
+	process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://mhucanada:${password}@cluster0.zya80.mongodb.net/note-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://mhucanada:${password}@cluster0.zya80.mongodb.net/note-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
 
 // schema tells mongoose how the note objects are stored in the database
 // the data stored in the DB is given a schema at the level of the application that defines the documents in any collection
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+	content: String,
+	date: Date,
+	important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
-
 
 /* const note = new Note({
   content: 'HTML is Easy',
@@ -30,11 +28,11 @@ const Note = mongoose.model('Note', noteSchema)
   important: true,
 }) */
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+Note.find({}).then((result) => {
+	result.forEach((note) => {
+		console.log(note)
+	})
+	mongoose.connection.close()
 })
 
 //this saves the note, and makes sure to close the connection through mongoose
